61a8f33e9e48a05a7ba756e0ad060efdb084e414
AlexanderNkn <yspresi@yandex.ru>
2022-04-12 23:05:11 +0300

    dokerized notifications

---
+++ notifications/kafka_rabbit_waiter.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ "$KAFKA_SERVICE" = "kafka" ]
+then
+    echo "Waiting for Kafka ..."
+
+    while ! nc -z $KAFKA_HOST $KAFKA_PORT; do
+      sleep 10
+    done
+
+    echo "Kafka started"
+fi
+
+if [ "$RABBIT_SERVICE" = "rabbit" ]
+then
+    echo "Waiting for RabbitMQ ..."
+
+    while ! nc -z $RABBIT_HOST $RABBIT_PORT; do
+      sleep 10
+    done
+
+    echo "RabbitMQ started"
+fi
+
+exec "$@"
